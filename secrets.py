a4516ff262097e3746f7e2ef7019d610db5a6040
dwightshepherd <107561013+dwightshepherd@users.noreply.github.com>
2022-06-15 09:17:27 -0500

    First commit of secrets file - secrets.py
    This file contains credentials for Linux Backup Server and Checkpoint FW

---
+++ secrets.py
@@ -0,0 +1,11 @@
+checkpoint_fw = {
+    "username": "admin",
+    "password": "admin",
+    "ip": "10.0.0.1"
+}
+
+backup_server = {
+    "username": "admin",
+    "password": "admin",
+    "ip": "10.0.0.2"
+}
